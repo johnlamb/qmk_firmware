dc6ff19a35367d3a60615bce09c62790cb6b2d1c
John Lamb <lamb.john.n@gmail.com>
2021-08-02 11:57:54 +0200

    Adjust crkbd

--- keyboards/crkbd/keymaps/johnlamb/keymap.c
+++ keyboards/crkbd/keymaps/johnlamb/keymap.c
@@ -129,9 +129,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         RESET, _______, ON_WIN, _______,    KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______,                        KC_F1,   KC_F2,   KC_F3, _______, _______, _______,\
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_LALT, _______,                      _______, _______, _______, _______, _______, _______,\
+      KC_LCTL, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______,                        KC_F1,   KC_F2,   KC_F3, _______, _______, _______,\
+  //|--------+-------+--------+--------+--------+---------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LSFT, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______,                      _______, _______, _______, _______, _______, _______,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI,   LOWER,  KC_SPC,     LT(MOUSE, KC_ENT),   RAISE, KC_LALT \
                                       //`--------------------------'  `--------------------------'
@@ -166,69 +166,69 @@ void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
   }
 }
 
-void matrix_init_user(void) {
-    #ifdef RGBLIGHT_ENABLE
-      RGB_current_mode = rgblight_config.mode;
-    #endif
-    //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
-    #ifdef SSD1306OLED
-        iota_gfx_init(!has_usb());   // turns on the display
-    #endif
-}
+// void matrix_init_user(void) {
+//     #ifdef RGBLIGHT_ENABLE
+//       RGB_current_mode = rgblight_config.mode;
+//     #endif
+//     //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
+//     #ifdef SSD1306OLED
+//         iota_gfx_init(!has_usb());   // turns on the display
+//     #endif
+// }
 
 //SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
-#ifdef SSD1306OLED
+//#ifdef SSD1306OLED
 
 // When add source files to SRC in rules.mk, you can use functions.
-const char *read_layer_state(void);
-const char *read_logo(void);
-void set_keylog(uint16_t keycode, keyrecord_t *record);
-const char *read_keylog(void);
-const char *read_keylogs(void);
+// const char *read_layer_state(void);
+// const char *read_logo(void);
+// void set_keylog(uint16_t keycode, keyrecord_t *record);
+// const char *read_keylog(void);
+// const char *read_keylogs(void);
 
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
 // void set_timelog(void);
 // const char *read_timelog(void);
 
-void matrix_scan_user(void) {
-   iota_gfx_task();
-}
+// void matrix_scan_user(void) {
+//    iota_gfx_task();
+// }
 
-void matrix_render_user(struct CharacterMatrix *matrix) {
-  if (is_master) {
-    // If you want to change the display of OLED, you need to change here
-    matrix_write_ln(matrix, read_layer_state());
-    matrix_write_ln(matrix, read_keylog());
-    //matrix_write_ln(matrix, read_keylogs());
-    //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-    //matrix_write_ln(matrix, read_host_led_state());
-    //matrix_write_ln(matrix, read_timelog());
-  } else {
-    matrix_write(matrix, read_logo());
-  }
-}
-
-void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
-  if (memcmp(dest->display, source->display, sizeof(dest->display))) {
-    memcpy(dest->display, source->display, sizeof(dest->display));
-    dest->dirty = true;
-  }
-}
-
-void iota_gfx_task_user(void) {
-  struct CharacterMatrix matrix;
-  matrix_clear(&matrix);
-  matrix_render_user(&matrix);
-  matrix_update(&display, &matrix);
-}
-#endif//SSD1306OLED
+// void matrix_render_user(struct CharacterMatrix *matrix) {
+//   if (is_master) {
+//     // If you want to change the display of OLED, you need to change here
+//     matrix_write_ln(matrix, read_layer_state());
+//     matrix_write_ln(matrix, read_keylog());
+//     //matrix_write_ln(matrix, read_keylogs());
+//     //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
+//     //matrix_write_ln(matrix, read_host_led_state());
+//     //matrix_write_ln(matrix, read_timelog());
+//   } else {
+//     matrix_write(matrix, read_logo());
+//   }
+// }
+// 
+// void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
+//   if (memcmp(dest->display, source->display, sizeof(dest->display))) {
+//     memcpy(dest->display, source->display, sizeof(dest->display));
+//     dest->dirty = true;
+//   }
+// }
+// 
+// void iota_gfx_task_user(void) {
+//   struct CharacterMatrix matrix;
+//   matrix_clear(&matrix);
+//   matrix_render_user(&matrix);
+//   matrix_update(&display, &matrix);
+// }
+// #endif//SSD1306OLED
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
-#ifdef SSD1306OLED
-    set_keylog(keycode, record);
-#endif
+// #ifdef SSD1306OLED
+//     set_keylog(keycode, record);
+// #endif
     // set_timelog();
   }
 
